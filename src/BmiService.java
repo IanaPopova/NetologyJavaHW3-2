a4f47c3766893d5a2d66a266644e04f84889d3cc
Яна Попова <ynappva@gmail.com>
2024-04-24 12:37:07 +0300

    BmiService updated

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
     public int calculate(int weightInKg, double heightInM) {
-        double bodyMassIndex = (int) (weightInKg / heightInM * heightInM);
+        double bodyMassIndex = (int) (weightInKg / (heightInM * heightInM));
         return (int) bodyMassIndex;
     }
 }
